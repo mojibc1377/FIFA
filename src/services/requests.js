09b9be0fbbbecd3295c3cbcf88d63c3136fa58b6
mojtaba <mojtaba@MacAroni.local>
2023-08-01 16:37:24 +0330

    withChargePannel

--- src/services/requests.js
+++ src/services/requests.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const request = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://fifa-backend-tgn1.onrender.com',
 
 });
 export const REQUEST = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://fifa-backend-tgn1.onrender.com',
 	headers: {"content-type": "application/json"}
 })
 // https://fifa-backend-tgn1.onrender.com
