447529bf875adae82406e2c4981d8e63e39d438d
mojtaba <mojtaba@MacAroni.local>
2023-08-05 14:42:37 +0330

    readyToTest

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,7 +4,7 @@ function Header (){
     return(
         <div className='header pt-1 '>
         <a href='/'><img className='header-logo h-11 pr-1' src='/images/logo/ea-logo.png' alt='header-logo'></img></a>
-        <p className='header-text font-mono italic font-medium text-white space-x-4'>FIFA MOJI</p>
+        <p className='header-text font-mono italic font-medium text-white space-x-4'>ChampsPlus</p>
         </div>
 
     )
