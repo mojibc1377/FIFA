d96c28269421d06c95253ef5cbb0cceb1076a11b
mojtaba <mojtaba@MacAroni.local>
2023-09-02 14:48:52 +0330

    paymentTest

--- src/services/requests.js
+++ src/services/requests.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const request = axios.create({
-    baseURL: 'https://fifa-backend-tgn1.onrender.com',
+    baseURL: 'http://localhost:8000',
 });
 export const REQUEST = axios.create({
-    baseURL: 'https://fifa-backend-tgn1.onrender.com',
+    baseURL: 'http://localhost:8000',
 	headers: {"content-type": "application/json"}
 })
 
